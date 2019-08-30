f6ad541f538d2dd31d6858f43e91533c74a7bffa
nFerrar <nicholas.ferrar@live.com>
2019-08-30 17:21:51 +1000

    removed unrelated code

--- PSData.h
+++ PSData.h
@@ -5,9 +5,6 @@
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 
-#include "Engine/DataTable.h"
-#include "RandomStream.h"
-
 #include "PSData.generated.h"
 
 
@@ -24,18 +21,6 @@ class NFPOPULATIONSYSTEM_API UPSData : public UBlueprintFunctionLibrary
 	////////////////////
 
 public:
-
-	UFUNCTION(BlueprintCallable, category = "nfPopulationSystem|Settings")
-		static bool DoesUseMiddleNames();
-
-	UFUNCTION(BlueprintPure, category = "nfPopulationSystem|Settings")
-		static UDataTable* GetFirstNamesData();
-
-	UFUNCTION(BlueprintPure, category = "nfPopulationSystem|Settings")
-		static UDataTable* GetMiddleNamesData();
-
-	UFUNCTION(BlueprintPure, category = "nfPopulationSystem|Settings")
-		static UDataTable* GetLastNamesData();
 
 	///Actual Setters and getter
 	//Setters
@@ -111,92 +96,5 @@ public:
 
 	UFUNCTION(BlueprintCallable, BlueprintInternalUseOnly)
 		static bool GetEnumByName(UObject* Target, FName VarName, uint8 &OutValue);
-
-	/*
-	UFUNCTION(BlueprintCallable, Category = "Population System|Helpers", CustomThunk, meta = (CustomStructureParam = "OutProp"))//, WorldContext = "Context"))//, DeterminesOutputType = "OutProp"))
-		static class UProperty* GetVariableByName(UObject* Target, UPARAM(ref) const FString VariableName, UPARAM(ref) UProperty*& OutProp);
-
-	DECLARE_FUNCTION(execGetVariableByName)
-	{
-		//Get properties and pointers from stack
-
-		//Gets UObject* to Target
-		Stack.StepCompiledIn<UObjectProperty>(NULL);
-		UObjectProperty* TargetProp = ExactCast<UObjectProperty>(Stack.MostRecentProperty);
-		UObject* Target = TargetProp->GetObjectPropertyValue(Stack.MostRecentPropertyAddress);
-
-		//Get FName to VariableName
-		Stack.StepCompiledIn<UStrProperty>(NULL);
-		UStrProperty* VarNameProp = ExactCast<UStrProperty>(Stack.MostRecentProperty);
-		FString StrValue = VarNameProp->GetPropertyValue(Stack.MostRecentPropertyAddress);
-		FName VariableName = *StrValue;
-
-		//Get UProperty to OutPropProp
-		//Stack.StepCompiledIn<UBoolProperty>(NULL);
-		Stack.StepCompiledIn<UProperty>(NULL);
-		UProperty* OutPropProp = Stack.MostRecentProperty;
-		//UBoolProperty* OutBoolProp = ExactCast<UBoolProperty>(OutPropProp);
-		//UFloatProperty* OutFloatProp = ExactCast<UFloatProperty>(OutPropProp);
-
-		Stack.StepCompiledIn<UObjectProperty>(NULL);
-		UObjectProperty* ContextProp = ExactCast<UObjectProperty>(Stack.MostRecentProperty);
-		UObject* ContextValue = TargetProp->GetObjectPropertyValue(Stack.MostRecentPropertyAddress);
-		
-		P_FINISH;
-
-		P_NATIVE_BEGIN;
-
-		if(Target && ContextValue)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("1 Valid Target and Context"));
-			UFloatProperty* OutFloatProp = FindField<UFloatProperty>(Target->GetClass(), VariableName);
-			if (OutFloatProp)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("2 Valid float property, looking for value"));
-				float OutFloat;
-				UFloatProperty* FloatProp = FindField<UFloatProperty>(Target->GetClass(), VariableName);  // try to find float property in Target named VarName
-				if (FloatProp) //if we found variable
-				{
-					UE_LOG(LogTemp, Warning, TEXT("3 found a float prop, confirming"));
-					OutFloat = FloatProp->GetPropertyValue_InContainer(Target);  // get the value from FloatProp
-
-					UE_LOG(LogTemp, Warning, TEXT("4 Context: %s"), *ContextValue->GetName());
-					//ExactCast<UFloatProperty>(OutPropProp)->SetPropertyValue_InContainer(ContextValue, OutFloat);
-					ExactCast<UFloatProperty>(OutPropProp)->SetPropertyValue_InContainer(ContextValue, OutFloat);
-					*(bool*)RESULT_PARAM = true;
-					return;
-				}
-			}
-
-			UBoolProperty* OutBoolProp = FindField<UBoolProperty>(Target->GetClass(), VariableName);
-			if (OutBoolProp)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("2 Valid bool property, looking for value"));
-				bool OutBool;
-				UBoolProperty* BoolProp = FindField<UBoolProperty>(Target->GetClass(), VariableName);  // try to find bool property in Target named VarName
-				if (BoolProp) //if we found variable
-				{
-					UE_LOG(LogTemp, Warning, TEXT("3 found a bool prop, confirming"));
-					OutBool = BoolProp->GetPropertyValue_InContainer(Target);  // get the value from FloatProp
-
-					UE_LOG(LogTemp, Warning, TEXT("4 Context: %s"), *ContextValue->GetName());
-					ExactCast<UBoolProperty>(OutPropProp)->SetPropertyValue_InContainer(ContextValue, OutBool);
-					*(bool*)RESULT_PARAM = true;
-					return;
-				}
-			}
-			else
-			{
-				UE_LOG(LogTemp, Warning, TEXT("5 Value Property Not Valid"));
-			}
-		}
-		OutPropProp = nullptr;
-		UE_LOG(LogTemp, Warning, TEXT("6 Found nothing, fail"));
-
-		*(bool*)RESULT_PARAM = false;
-		
-		P_NATIVE_END;
-	}
-	*/
 
 };
